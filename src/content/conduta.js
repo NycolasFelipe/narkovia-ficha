519e8def5934dbf2e60ca5ab4c50e09bf910de46
Nycolas Felipe <nycolaslourenco.contato@gmail.com>
2024-02-28 19:22:36 -0300

    Criar Ficha: Correção de url para acesso dos conteúdos de conduta

--- src/content/conduta.js
+++ src/content/conduta.js
@@ -1,11 +1,34 @@
-import * as files from "./condutas" assert { type: 'json' };
+const files = [
+  "conduta-abastado.json",
+  "conduta-alopata.json",
+  "conduta-alquimista.json",
+  "conduta-aristocrata.json",
+  "conduta-cacador-da-noite.json",
+  "conduta-cacador-de-cabecas.json",
+  "conduta-eremita.json",
+  "conduta-explorador.json",
+  "conduta-expurgador.json",
+  "conduta-falsificador.json",
+  "conduta-filhas-da-primeira-luz.json",
+  "conduta-inspetor.json",
+  "conduta-inventor.json",
+  "conduta-ladrao.json",
+  "conduta-mestre-de-vislumbre.json",
+  "conduta-mestre-mun'kai.json",
+  "conduta-pirata.json",
+  "conduta-professor-estudioso.json",
+  "conduta-profeta.json",
+  "conduta-sacerdote.json",
+  "conduta-servo-de-honra.json",
+  "conduta-trapaceiro_golpista.json",
+  "conduta-tribal.json"
+]
 
 const condutas = [];
 const pathname = window.location.origin;
 const path = pathname.includes("github") ? pathname + "/narkovia-ficha" : pathname;
 
-$.each(files.default, (index, conduta) => {
-  console.log(`${path}/src/content/condutas/${conduta}`);
+$.each(files, (index, conduta) => {
   fetch(`${path}/src/content/condutas/${conduta}`)
     .then((response) => response.json())
     .then((json) => condutas.push(json));
