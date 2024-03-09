70226f93ec2dd4fa7af45622801922058f2fdddd
Nycolas Felipe <nycolaslourenco.contato@gmail.com>
2024-03-09 19:53:51 -0300

    Adição de animação de loading para a página de criar ficha

--- src/scripts/criar-ficha/script.js
+++ src/scripts/criar-ficha/script.js
@@ -6,7 +6,10 @@ import changeTamanho from "./changeTamanho.js";
 import changeAtributo from "./changeAtributo.js";
 import slickSlider from "../../scripts/common/slickSlider.js";
 import validaPasso from "./validaPasso.js";
+import handleLoading from "./handleLoading.js";
 // import ___debug from "./___debug.js";
+
+handleLoading();
 
 $(document).ready(() => {
   changeAncestralidade();
